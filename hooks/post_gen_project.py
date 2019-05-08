1b0c3eff87f03415cd61500f7e78cbc2ab538971
xesscorp <xesscorp@xess.com>
2019-05-08 18:13:55 -0400

    Both Click and argparse CLIs end up in cli.py.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,6 +7,9 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
+def rename_file(from_path, to_path):
+    os.rename(os.path.join(PROJECT_DIRECTORY, from_path),
+              os.path.join(PROJECT_DIRECTORY, to_path))
 
 if __name__ == '__main__':
 
@@ -18,16 +21,22 @@ if __name__ == '__main__':
         remove_file('tests/__init__.py')
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
-        main_file = os.path.join('{{ cookiecutter.project_slug }}', '__main__.py')
-        remove_file(main_file)
+        click_file = os.path.join('{{ cookiecutter.project_slug }}', 'click_cli.py')
+        remove_file(click_file)
+        argp_file = os.path.join('{{ cookiecutter.project_slug }}', 'argp_cli.py')
+        remove_file(argp_file)
     elsif '{{ cookiecutter.command_line_interface|lower }}' == 'argparse':
+        click_file = os.path.join('{{ cookiecutter.project_slug }}', 'click_cli.py')
+        remove_file(click_file)
+        argp_file = os.path.join('{{ cookiecutter.project_slug }}', 'argp_cli.py')
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
+        rename(argp_file, cli_file)
     elsif '{{ cookiecutter.command_line_interface|lower }}' == 'click':
-        main_file = os.path.join('{{ cookiecutter.project_slug }}', '__main__.py')
-        remove_file(main_file)
+        argp_file = os.path.join('{{ cookiecutter.project_slug }}', 'argp_cli.py')
+        remove_file(argp_file)
+        click_file = os.path.join('{{ cookiecutter.project_slug }}', 'click_cli.py')
+        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
+        rename(click_file, cli_file)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
