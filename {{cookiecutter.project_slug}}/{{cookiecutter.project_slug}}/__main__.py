fb6394485edfd9cd60fd9b12cf153d41640c112e
xesscorp <xesscorp@xess.com>
2019-05-08 17:27:55 -0400

    Removed unneeded items from CLI.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -1,46 +1,15 @@
 # -*- coding: utf-8 -*-
 
-# MIT license
-# 
-# Copyright (C) {{ cookiecutter.year }} by { cookiecutter.full_name }
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
-
 import argparse
 import os
 import shutil
 import sys
 import logging
-from .{{ cookiecutter.pypi_name }} import *
-from .pckg_info import version
+from .{{ cookiecutter.project_name }} import *
 
 ###############################################################################
 # Command-line interface.
 ###############################################################################
-
 
 def main():
     parser = argparse.ArgumentParser(
@@ -141,7 +110,7 @@ def main():
         else:
             inc_fields.append(f)
 
-    {{ cookiecutter.pypi_name }}(extract_filenames=args.extract,
+    {{ cookiecutter.project_name }}(extract_filenames=args.extract,
             insert_filenames=args.insert,
             inc_field_names=inc_fields,
             exc_field_names=exc_fields)
